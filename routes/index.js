1ca55eab69236097b1a8a40eeb26a877921ae78e
sungyeon-0975 <sustain98@naver.com>
2021-04-05 16:18:48 +0900

    로그인 기능 추가

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,21 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 
+router.post('/endpoint-for-get-user-info',function(req,res,next){
+  var body = req.body;
+  console.log(body.params);
+  if (body.params.uid == "admin" && body.params.password == "admin"){
+    res.send({uid : 'admin'});
+  }else{
+    res.send();
+  }
+})
+router.post('/endpoint-for-is-finished', function(req,res,next){
+  var body = req.body;
+  if (body.params.uid == 'admin'){
+    res.send({uid : 'admin'})
+  }
+})
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname,'../public','index.html'));
--- routes/users.js
+++ routes/users.js
@@ -6,15 +6,15 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/login', function(req, res, next){
-  res.send('login check');
-  var body = req.body;
-  console.log(req.body)
-  if(body.id == "admin"){//아이디 검증
-    if(body.password == "admin"){//해시검증 (passwd, isolate)
-      console.log("어드민이 맞음");
-    }
-  }
-})
+// router.post('/login', function(req, res, next){
+//   res.send('login check');
+//   var body = req.body;
+//   console.log(req.body)
+//   if(body.id == "admin"){//아이디 검증
+//     if(body.password == "admin"){//해시검증 (passwd, isolate)
+//       console.log("어드민이 맞음");
+//     }
+//   }
+// })
 
 module.exports = router;
